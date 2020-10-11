2a97f66f30728585997193488332430178a2bae9
DenisMutinda <dennismutinda007@gmail.com>
2020-10-11 18:16:46 +0200

    Added Alpha

--- misc/scripts/autostart.sh
+++ misc/scripts/autostart.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 xmodmap -e "pointer = 3 2 1 4 5 6 7 8 9 10" &
-pkill slstatus
+pkill slstatus &
+pkill picom &
 sleep 2
 feh --bg-fill ~/.config/misc/walls/mando.jpg ~/.config/misc/walls/desert-eye.jpg &
 numlockx on &
